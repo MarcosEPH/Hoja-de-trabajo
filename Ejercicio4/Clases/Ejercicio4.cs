7c817ce53a1a4b675fbb8fa0fdb573272a904ded
Fernando Mazariegos <nandomf46@gmail.com>
2025-07-30 23:16:34 -0600

    ejercicios del 4 al 6

--- Ejercicio4/Clases/Ejercicio4.cs
+++ Ejercicio4/Clases/Ejercicio4.cs
@@ -1,12 +1,50 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Ejercicio4.Clases
+abstract class Vehiculo
 {
-    internal class Ejercicio4
+    public abstract void Encender();
+    public abstract void Conducir();
+}
+
+class Carro : Vehiculo
+{
+    public override void Encender()
     {
+        Console.WriteLine("Encendiendo carro");
+    }
+
+    public override void Conducir()
+    {
+        Console.WriteLine("Conduciendo carro");
+    }
+}
+
+class Motocicleta : Vehiculo
+{
+    public override void Encender()
+    {
+        Console.WriteLine("Encendiendo motocicleta");
+    }
+
+    public override void Conducir()
+    {
+        Console.WriteLine("Conduciendo motocicleta");
+    }
+}
+
+class Ejercicio4Main
+{
+    static void Main()
+    {
+        Vehiculo[] vehiculos = {
+            new Carro(),
+            new Motocicleta()
+        };
+
+        foreach (var v in vehiculos)
+        {
+            v.Encender();
+            v.Conducir();
+        }
     }
 }
